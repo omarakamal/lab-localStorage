7a47063b4209fb67a3c01e01272b81e1f61287cc
Dimitriy-macho <ddyudin02@gmail.com>
2024-06-20 21:09:32 +0100

    done

--- listing-page/listing-page.js
+++ listing-page/listing-page.js
@@ -1,12 +1,29 @@
 function showListing(){
-   let listing =  localStorage.getItem('listing')
-
+   let listing = JSON.parse(localStorage.getItem('listing'))
 
    console.log('listing',listing)
    console.log('listing')
 
+   DivOne = document.getElementById("one-item-container")
+
+
+    let img = document.createElement("img")
+    let h2 = document.createElement("h2")
+    let p = document.createElement("p")
+
+    img.src = listing.picture ;
+    h2.innerHTML = listing.title;
+    p.innerHTML = listing.price;
+
+    DivOne.appendChild(img)
+    DivOne.appendChild(h2)
+    DivOne.appendChild(p)
 }
 
 window.addEventListener('load',()=>{
     showListing()
 })
+
+
+
+
