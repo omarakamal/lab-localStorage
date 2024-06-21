28f306af3acc268ab469aab04dd0dfa1ab10436e
Mary Judi Kavitha Karunakaran <kavitha.rijesh@gmail.com>
2024-06-21 20:18:31 +0200

    listing updated - still working on shoppingcart

--- homepage/index.js
+++ homepage/index.js
@@ -3,10 +3,12 @@
 function seeListing(title , price, picture){
 console.log(title , price, picture)
 
+//save to local storage
+
 localStorage.setItem('listing',JSON.stringify({title,price,picture}))
 
+//take the product to see-listing page 
 
-window.location.href = '../listing-page/listingPage.html';
-
+window.location.href = '../listing-page/listingPage.html'
 
 }
