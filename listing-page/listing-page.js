9d2b0012f2f3f8dc7622657153472c96b1bf5663
Mikko Peltonen <mikko.inno.peltonen@gmail.com>
2024-06-21 20:29:53 +0300

    local Storage working

--- listing-page/listing-page.js
+++ listing-page/listing-page.js
@@ -1,8 +1,22 @@
 function showListing(){
 
-
+   let listing = JSON.parse(localStorage.getItem('listing'))
    console.log('listing',listing)
    console.log('listing')
+
+   let itemListedDiv = document.getElementById('one-item-container')
+
+   titleElement = document.createElement('h2')
+   priceElement = document.createElement('p')
+   pictureElement = document.createElement('img')
+
+   titleElement.innerHTML = listing.title
+   priceElement.innerHTML = listing.price
+   pictureElement.src = listing.picture
+
+   itemListedDiv.appendChild(pictureElement)
+   itemListedDiv.appendChild(titleElement)
+   itemListedDiv.appendChild(priceElement)
 
 }
 
