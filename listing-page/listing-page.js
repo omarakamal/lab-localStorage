65ac7e35ed024a9cdd2e4314a6a957a668f41640
Daniel Martel <danielmartelmarrero@gmail.com>
2024-06-24 20:15:05 +0100

    Mandatory part finished

--- listing-page/listing-page.js
+++ listing-page/listing-page.js
@@ -1,9 +1,21 @@
 function showListing(){
+    let listingItem = JSON.parse(localStorage.getItem('listing'))
 
+    let h1    = document.createElement("h1")
+    let img   = document.createElement("img")
+    let p     = document.createElement("p")
+    let newDiv = document.getElementById("one-item-container")
 
-   console.log('listing',listing)
-   console.log('listing')
-
+    h1.innerHTML= listingItem.title
+    img.src     = listingItem.picture
+    img.alt     = listingItem.title
+    p.innerHTML = `$ ${listingItem.price}`
+    console.log(img)
+    newDiv.appendChild(h1)
+    newDiv.appendChild(img)
+    newDiv.appendChild(p)
+    
+    console.log(newDiv)
 }
 
 window.addEventListener('load',()=>{
